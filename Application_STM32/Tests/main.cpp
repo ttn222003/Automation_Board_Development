ebe328665abb835d1fc2c0cfecb3649df2255c25
ttn222003 <truongtrongnhan.222003@gmail.com>
2025-10-23 23:00:44 +0700

    Add New Makefile for Testing Core Logic

--- Application_STM32/Tests/main.cpp
+++ Application_STM32/Tests/main.cpp
@@ -2,7 +2,12 @@
  * main.cpp
  *
  *  Created on: Oct 23, 2025
- *      Author: ASUS
+ *      Author: TTN
  */
 
+#include "CppUTest/CommandLineTestRunner.h"
 
+int main(int argc, char** argv)
+{
+	return CommandLineTestRunner::RunAllTests(argc, argv);
+}
