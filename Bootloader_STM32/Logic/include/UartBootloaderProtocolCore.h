0d31e6c8cf1f94225574876d8bc89764d3d3e01b
ttn222003 <truongtrongnhan.222003@gmail.com>
2025-10-30 23:08:46 +0700

    Finish test all cmd code of protocol

--- Bootloader_STM32/Logic/include/UartBootloaderProtocolCore.h
+++ Bootloader_STM32/Logic/include/UartBootloaderProtocolCore.h
@@ -14,9 +14,21 @@
 
 enum CommandCode
 {
-    GET_CMD     = 0x00,
-    GET_VERSION = 0x01,
-    GET_ID      = 0x02,
+    GET_CMD             = 0x00,
+    GET_VERSION         = 0x01,
+    GET_ID              = 0x02,
+    READ_MEM            = 0x11,
+    GO_CMD              = 0x21,
+    WRITE_MEM           = 0x31,
+    ERASE_MEM           = 0x43,
+    EXT_ERASE_MEM       = 0x44,
+    WRITE_PROTECT       = 0x63,
+    WRITE_UNPROTECT     = 0x73,
+    READOUT_PROTECT     = 0x82,
+    READOUT_UNPROTECT   = 0x92,
+    GET_CHECKSUM        = 0xA1,
+    SPECIAL_CMD         = 0x50,
+    EXT_SPECIAL_CMD     = 0x51
 };
 
 unsigned char CheckCommandCode(unsigned char received_data[]);
--- Bootloader_STM32/Tests/tests/BootloaderProtocolCoreCheckCmdCodeTest.cpp
+++ Bootloader_STM32/Tests/tests/BootloaderProtocolCoreCheckCmdCodeTest.cpp
@@ -28,3 +28,87 @@ TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeGetID)
     unsigned char command_code = CheckCommandCode(data);
     CHECK_EQUAL(GET_ID, command_code);
 }
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeReadMemory)
+{
+    unsigned char data[2] = {0x11, 0xEE};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(READ_MEM, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeGo)
+{
+    unsigned char data[2] = {0x21, 0xDE};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(GO_CMD, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeWriteMemory)
+{
+    unsigned char data[2] = {0x31, 0xCE};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(WRITE_MEM, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeEraseMemory)
+{
+    unsigned char data[2] = {0x43, 0xBC};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(ERASE_MEM, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeExtendEraseMemory)
+{
+    unsigned char data[2] = {0x44, 0xBB};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(EXT_ERASE_MEM, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeWriteProtect)
+{
+    unsigned char data[2] = {0x63, 0x9C};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(WRITE_PROTECT, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeWriteUnprotect)
+{
+    unsigned char data[2] = {0x73, 0x8C};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(WRITE_UNPROTECT, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeReadoutProtect)
+{
+    unsigned char data[2] = {0x82, 0x7D};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(READOUT_PROTECT, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeReadoutUnprotect)
+{
+    unsigned char data[2] = {0x92, 0x6D};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(READOUT_UNPROTECT, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeGetChecksum)
+{
+    unsigned char data[2] = {0xA1, 0x5E};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(GET_CHECKSUM, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeSpecialCmd)
+{
+    unsigned char data[2] = {0x50, 0xAF};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(SPECIAL_CMD, command_code);
+}
+
+TEST(BootloaderProtocolCoreCheckCmdCode, CheckCmdCodeExtendSpecialCmd)
+{
+    unsigned char data[2] = {0x51, 0xAE};
+    unsigned char command_code = CheckCommandCode(data);
+    CHECK_EQUAL(EXT_SPECIAL_CMD, command_code);
+}
