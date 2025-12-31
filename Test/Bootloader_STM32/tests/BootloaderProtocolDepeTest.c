00b00acd26bc16e48e0c5018e6a81ed5d2de0fd4
ttn222003 <truongtrongnhan.222003@gmail.com>
2025-12-31 22:05:51 +0700

    Update pattern rules of Makefile

--- Test/Bootloader_STM32/tests/BootloaderProtocolDepeTest.c
+++ Test/Bootloader_STM32/tests/BootloaderProtocolDepeTest.c
@@ -1,6 +1,8 @@
 #include "unity.h"
 #include "unity_color.h"
 #include "UartBootloaderProtocolDepenedencies.h"
+#include "MockUartDriver.h"
+#include "MockTimerDriver.h"
 
 void setUp(void)
 {
@@ -14,6 +16,25 @@ void tearDown(void)
 
 void TransmittEightDataToHostTest(void)
 {
+    HandleAckForTransmission(TransmittedDataToHost);
+    // Expect
+    DelayMs_Expect(1);
+    UartTransmittOneByteData_Expect(RESPONE_HANDSHAKE);
+    DelayMs_Expect(1);
+    UartTransmittOneByteData_Expect(8);
+    DelayMs_Expect(1);
+    UartTransmittOneByteData_Expect(ACK);
+    DelayMs_Expect(1);
+    UartTransmittOneByteData_Expect(0);
+    DelayMs_Expect(1);
+    UartTransmittOneByteData_Expect(105);
+    DelayMs_Expect(1);
+    UartTransmittOneByteData_Expect(13);
+    DelayMs_Expect(1);
+    UartTransmittOneByteData_Expect(223);
+    DelayMs_Expect(1);
+    UartTransmittOneByteData_Expect(114);
+
     TransmittDataToHost(8);
 }
 
@@ -21,6 +42,7 @@ int main(void)
 {
     printf(COL_YELLOW "==== START DEPE TEST ====\n" COL_RESET);
     UNITY_BEGIN();
+    RUN_TEST(TransmittEightDataToHostTest);
     printf(COL_YELLOW "==== END DEPE TEST ====\n" COL_RESET);
     return UNITY_END();
 }
