3b9e820fedf8f0825ca2bc900bf738d920b820e0
ttn222003 <truongtrongnhan.222003@gmail.com>
2026-01-02 20:52:34 +0700

    Update src of Get Command

--- Bootloader_STM32/Platform/STM32/UartDriver.c
+++ Bootloader_STM32/Platform/STM32/UartDriver.c
@@ -7,11 +7,6 @@
 
 #include "UartDriver.h"
 
-void DelayMs(uint32_t time_delay)
-{
-	HAL_Delay(time_delay);
-}
-
 void UartTransmittOneByteData(uint8_t transmitted_data)
 {
 	HAL_UART_Transmit(&huart1, &transmitted_data, 1, 1);
